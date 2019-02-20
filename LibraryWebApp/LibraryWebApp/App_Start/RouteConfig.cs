ca024019bc94be5cb048cbf20def1aeebfb2124c
ancajeckel <anca.jeckel@yahoo.com>
2019-02-20 23:24:43 +0200

    New controller Author, image on the header

--- LibraryWebApp/LibraryWebApp/App_Start/RouteConfig.cs
+++ LibraryWebApp/LibraryWebApp/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace LibraryWebApp
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Author",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Author", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
